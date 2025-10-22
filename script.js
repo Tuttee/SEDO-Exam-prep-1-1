09abd866bd4ad669b12cace1c5af04fe55e89c07
Tuttee <111608871+Tuttee@users.noreply.github.com>
2025-10-22 22:49:52 +0300

    change 1
    Change 2
    
    Change 3

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ function subtract(a, b) {
   return a - b;
 }
 
+//change 1
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = add(a, b);
 });
 
+//Change 2
 document.getElementById('btn-subtract').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = subtract(a, b);
 });
+
+//Change 3
